96e6da0448a0ae341c2a7f3a237617c5010e73af
NiekDP <niekdepraeter@me.com>
2023-12-06 13:54:25 +0100

    hotjarcodeee

--- HeadComponent.js
+++ HeadComponent.js
@@ -62,16 +62,6 @@ export default class HeadComponent extends Component {
 				{this.state.loaded && <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@3.9.97/css/materialdesignicons.min.css" />}
 				{this.state.loaded && <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />}
 				<!-- Hotjar Tracking Code for https://topacademy-liard.vercel.app/home -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3763529,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
 			</Head>
 			<Script id="tawktoscriptwrapper" dangerouslySetInnerHTML={{
 				__html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
